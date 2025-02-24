7fdc8cd40877c89435676d0a9f41f8071197e74a
Yudha Dewantara <110522695+ydewantaraa@users.noreply.github.com>
2025-02-24 19:49:35 +0700

    Update Pertemuan_2.ino

--- Basic/Pertemuan_2.ino
+++ Basic/Pertemuan_2.ino
@@ -1,3 +1,7 @@
+#define BLYNK_TEMPLATE_ID "TMPL6nWWjZeah"
+#define BLYNK_TEMPLATE_NAME "Led Blink"
+#define BLYNK_AUTH_TOKEN "P8GicZw5gH6MlyTwCRMrsRjjobcxOnov"
+
 #define BLYNK_PRINT Serial
 #include <WiFi.h>
 #include <BlynkSimpleEsp32.h>
@@ -8,17 +12,20 @@ char pass[] = "Password_WiFi";
 
 const int ledPin = 2;
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
     Blynk.begin(auth, ssid, pass);
     pinMode(ledPin, OUTPUT);
 }
 
-BLYNK_WRITE(V0) {
+BLYNK_WRITE(V0)
+{
     int buttonState = param.asInt();
     digitalWrite(ledPin, buttonState);
 }
 
-void loop() {
+void loop()
+{
     Blynk.run();
 }
